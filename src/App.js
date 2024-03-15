001557b360876ce68d8f5abd152893b53a16526b
anminner <minner.ashley.n@gmail.com>
2024-03-15 17:27:04 -0500

    restructured files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import LoginForm from './LoginForm';
+import Navigation from './Navigation';
+
+function App() {
+    return (
+        <div className="App">
+            <Navigation />
+            <LoginForm />
+        </div>
+    );
+}
+
+export default App;
+
---
+++ src/LoginForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './LoginForm.css'; // Import CSS file for styling
+
+const LoginForm = () => {
+    return (
+        <div className="login-form">
+            <h3>Log In</h3>
+            <form>
+                <div className="form-group">
+                    <input type="text" placeholder="Username" />
+                </div>
+                <div className="form-group">
+                    <input type="password" placeholder="Password" />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default LoginForm;
